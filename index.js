74861c5c15b0a3375cbe139b2db421ff6d808298
Matej Bouska <bouska.matej@gmail.com>
2019-10-14 20:36:40 +0200

    Download given number of images by tag

--- index.js
+++ index.js
@@ -1,44 +1,105 @@
 const puppeteer = require('puppeteer');
+const https = require('https');
+const fs = require("fs");
 
 const INSTAGRAM_URL = (tag) => `https://www.instagram.com/explore/tags/${tag}/`;
 const TAG = "bmw";
 
-(async () => {
+
+
+const asyncBrowser = async (url, headless = false, viewPort = { width: 1366, height: 1440 }) => {
     /* Initiate the Puppeteer browser */
     const browser = await puppeteer.launch({
-        headless: false
+        headless: headless
     });
     const page = await browser.newPage();
-    await page.setViewport({ width: 1366, height: 1440 });
-    /* Go to the Instagram page and wait for it to load */
-    await page.goto(INSTAGRAM_URL(TAG), { waitUntil: 'networkidle0' });
-    await autoScroll(page);
+    await page.setViewport(viewPort);
+    await page.goto(url, { waitUntil: 'networkidle0' });
+    return { "browser": browser, "firstPage": page };
 
-    var a = await page.evaluate((sel) => {
-        let elements = Array.from(document.querySelectorAll(sel));
-        let links = elements.map(element => {
-            return element.href
-        })
-        return links;
-    }, 'article > div:nth-child(3) a');
-    console.log(a);
-})();
-
-async function autoScroll(page){
-    await page.evaluate(async () => {
-        await new Promise((resolve, reject) => {
-            var totalHeight = 0;
-            var distance = 100;
-            var timer = setInterval(() => {
-                var scrollHeight = document.body.scrollHeight;
-                window.scrollBy(0, distance);
-                totalHeight += distance;
-
-                if(totalHeight >= scrollHeight){
-                    clearInterval(timer);
-                    resolve();
-                }
-            }, 100);
-        });
-    });
 }
+const asyncgetIstaPosts = async (page, selector, reqPostCount) => {
+    let posts = await page.evaluate(async ({ selector, reqPostCount }) => {
+        let links = [];
+        counter = 0;
+        return new Promise((resolve, reject) => {
+            var i = setInterval(() => {
+                let selElement = document.querySelector(selector);
+                selElement.scrollIntoView();
+                links.push(selElement.href);
+                selElement.remove();
+                counter++;
+                if (counter >= reqPostCount) {
+                    clearInterval(i);
+                    resolve(links);
+                }
+            }, 50);
+        }).then((data) => {
+            return data;
+        });
+
+    }, { selector, reqPostCount });
+    return posts;
+}
+
+
+const newTab = async (browser, url) => {
+    let tab = await browser.newPage();
+    await tab.goto(url, { waitUntil: 'networkidle0' });
+    return tab;
+}
+
+const getInstaPostSrc = async (page) => {
+    let imageUrl = await page.evaluate(() => {
+        let img = document.querySelector("img[srcset]");
+        if (img.alt != "Instagram") {
+            return img.src
+        }
+    });
+    return imageUrl;
+}
+
+const downloadPosts = async (browser, posts, count) => {
+    let i = 0;
+    let counter = 0;
+    while (counter < count) {
+        i++;
+        const photoTab = await newTab(browser, posts[i]);
+        const src = await getInstaPostSrc(photoTab);
+        photoTab.close();
+        if (src) {
+            counter++;
+            if (!fs.existsSync(TAG)) {
+                fs.mkdirSync(TAG);
+            }
+            result = await download(src, `./${TAG}/image-${counter}.png`);
+            if (result === true) {
+                console.log('Success:', posts[i], 'has been downloaded successfully.');
+                i++;
+            } else {
+                console.log('Error:', posts[i], 'was not downloaded.');
+                console.error(result);
+            }
+        }
+    }
+}
+
+const download = (url, destination) => new Promise((resolve, reject) => {
+    const file = fs.createWriteStream(destination);
+    https.get(url, response => {
+        response.pipe(file);
+        file.on('finish', () => {
+            file.close(resolve(true));
+        });
+    })
+        .on('error', error => {
+            fs.unlink(destination);
+            reject(error.message);
+        });
+});
+
+(async () => {
+    const browser = await asyncBrowser(INSTAGRAM_URL(TAG), false);
+    const posts = await asyncgetIstaPosts(browser.firstPage, 'article > div:nth-child(3) a', 150);
+    downloadPosts(browser.browser, posts, 20);
+})();
---
+++ test.js
@@ -0,0 +1,5 @@
+return new Promise((resolve, reject) => {
+    var i = setInterval(function () {
+        console.log("loguju");
+    }, 2000)
+})
