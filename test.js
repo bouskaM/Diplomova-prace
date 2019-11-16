5584bc59f1b73e2a0b1ec43037132fb304c75b0b
Matej Bouska <bouska.matej@gmail.com>
2019-11-16 12:49:21 +0100

    example

--- test.js
+++ test.js
@@ -1,3 +1,10 @@
 
- const scraper = require("./instagramScraper/scraper");
- scraper(5, "car", false, false);
+const scraper = require("./instagramScraper/scraper");
+//Download 4 images of
+//car
+//headless (false)
+//fastMode (true)
+scraper(4, "car", false, true);
+
+//Files are in ./scraped/car folder
+//Data is in db.json file
